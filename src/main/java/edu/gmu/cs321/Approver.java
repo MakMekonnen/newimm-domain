00cb638127d335ea7ff0128c4da086af38f11adf
Anon214 <anonrahman120@gmail.com>
2024-10-28 21:25:32 -0400

    added new state and added submit for approver class

--- src/main/java/edu/gmu/cs321/Approver.java
+++ src/main/java/edu/gmu/cs321/Approver.java
@@ -1,11 +1,13 @@
 package edu.gmu.cs321;
 
+import static edu.gmu.cs321.State.*;
+
 public class Approver extends Employee{
     public Approver(String eid, String name) {
         super(eid, name);
     }
 
     public void submit(Form form) {
-
+        form.set_state(COMPLETE_STATE);
     }
 }
--- src/main/java/edu/gmu/cs321/State.java
+++ src/main/java/edu/gmu/cs321/State.java
@@ -3,5 +3,6 @@ package edu.gmu.cs321;
 public enum State {
     DATA_ENTRY_STATE,
     REVIEWER_STATE,
-    APPROVER_STATE
+    APPROVER_STATE,
+    COMPLETE_STATE
 }
