729fbcebba9a5534764d0c8b44a33ed76f1c10c6
Anon214 <anonrahman120@gmail.com>
2024-10-30 18:57:09 -0400

    added javafx library

---
+++ src/main/java/edu/gmu/cs321/Screen.java
@@ -0,0 +1,11 @@
+package edu.gmu.cs321;
+
+import javafx.application.Application;
+
+public class Screen
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
